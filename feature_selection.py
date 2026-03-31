df997f23a4240e7b137d0033cde5f4f6a7b15b79
anastasiakuznietsova <anastasiia.kuznietsova.shi.2023@lpnu.ua>
2026-03-31 18:56:28 +0300

    feature: split method into two

--- feature_selection.py
+++ feature_selection.py
@@ -1,13 +1,19 @@
 from pyspark.sql import DataFrame
 
-def select_and_split_data(df: DataFrame, seed: int = 42):
+def select_columns(df: DataFrame):
     cols = ['emotion', 'text', 'Explicit', 'Length', 'Genre', 'Key', 'Tempo',
             'Loudness (db)', 'Time signature', 'Energy', 'Danceability',
             'Positiveness', 'Speechiness', 'Liveness', 'Acousticness', 'Instrumentalness']
 
     df_selected = df.select(*cols)
 
-    train_df, val_df, test_df = df_selected.randomSplit([0.7, 0.15, 0.15], seed=seed)
+    num_cols = len(df_selected.columns)
+    print(f"Dataframe columns: {num_cols}")
+
+    return df_selected
+
+def split_data(df: DataFrame, seed: int = 42):
+    train_df, val_df, test_df = df.randomSplit([0.7, 0.15, 0.15], seed=seed)
 
     num_cols = len(train_df.columns)
     print(f"Train shape: ({train_df.count()}, {num_cols})")
