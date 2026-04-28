1258a369d24b71a79a12a48c3387b8e5c88e6645
anastasiakuznietsova <anastasiia.kuznietsova.shi.2023@lpnu.ua>
2026-04-29 00:46:57 +0300

    bugfix: removed redundant code

--- business_questions/bq_kuznietsova.py
+++ business_questions/bq_kuznietsova.py
@@ -1,15 +1,6 @@
-from pyspark.sql import functions as F
 from pyspark.sql.window import Window
-
-from pyspark.sql import SparkSession
-from itertools import chain
 from pyspark.sql import functions as F
 
-from data_pipeline import load_and_validate_music_data
-from data_cleaning import clean_music_data
-
-from feature_selection import select_columns, split_data
-from data_typing import transform_and_scale_data
 
 #1
 def check_avg_positiveness_with_high_energy(df):
@@ -83,33 +74,3 @@ def high_energy_explicit_word_count(df):
         ))
     comparison_df.show()
     comparison_df.explain()
-
-spark = SparkSession.builder \
-        .appName("MusicPipeline") \
-        .config("spark.driver.memory", "4g") \
-        .config("spark.executor.memory", "4g") \
-        .getOrCreate()
-
-spark.sparkContext.setLogLevel("WARN")
-
-file_path = "../raw_data/spotify_dataset.csv"
-
-
-df = load_and_validate_music_data(spark, file_path)
-# df.show(5)
-
-df = clean_music_data(df)
-df = select_columns(df)
-df = transform_and_scale_data(df)
-
-mapping = {'sadness': 1, 'joy': 2, 'love': 3, 'surprise': 4, 'anger': 5, 'fear': 6}
-reverse_mapping = {v: k for k, v in mapping.items()}  # Буде {1: 'sadness', 2: 'joy', ...}
-mapping_expr = F.create_map([F.lit(x) for x in chain(*reverse_mapping.items())])
-df = df.withColumn("emotion", mapping_expr[F.col("emotion")])
-
-check_avg_positiveness_with_high_energy(df)
-genre_loudness_outliers(df)
-unstable_emotions(df)
-genres_energy_outliers(df)
-emotions_lyrics_density(df)
-high_energy_explicit_word_count(df)
